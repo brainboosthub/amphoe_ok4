0838228fad64bd5d38fe83d3f5cae3f497ffeb5e
brainboosthub <mintana9990@gmail.com>
2026-08-21 13:16:50 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbxnBOLJyV6BfeBcK66B9v3u3TznReLIfP-mKfM2J7I2c-GFLpAw8ukrFALcO9ZjHrZTMQ/exec';
+  'https://script.google.com/macros/s/AKfycbyvwOkpjuQQKozaz5Z2hOfJ19VRJup0Eqo3UWdV9YitnpI-5t8UihatA9C2Ncgq5rfwxQ/exec';
